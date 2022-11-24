97c1dc20bcd3a56b6bb3e5625cc6dbaf468a36e6
Youngwook Go <113055584+Youngwook-Go@users.noreply.github.com>
2022-11-24 04:54:58 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,39 @@
+// Created by: Youngwook
+// Created on: NOV 2022
+//
+// This function gets random number and user number
+// This function compares random number and user number and answer.
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // input
+        int input;
+
+        Console.WriteLine("This program gets user age and answer properly.\n");
+
+        Console.Write("Enter your age : "); input = Convert.ToInt32(Console.ReadLine());
+
+        if (input >= 17 )
+        {
+            Console.WriteLine("You can see an R movie alone.\n");
+        } 
+        else if (input >= 13) 
+        {
+            Console.WriteLine("You can see a PG-13 movie alone");
+        }
+        else if (input >= 5)
+        {
+            Console.WriteLine("You can see a G or PG movie alone");
+        } 
+        else {
+            Console.WriteLine("You're too young for most thing");
+        }
+
+        Console.WriteLine("Thanks for verifyng your age");
+        Console.WriteLine("\ndone.");
+    }
+}
